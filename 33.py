349be78cdb0bb7924a656c1b3ab154b283e864a3
Sera Wi <win10790@gmail.com>
2020-05-17 23:39:24 -0400

    Create 33.py

---
+++ 33.py
@@ -0,0 +1,28 @@
+# Time complexity : O(logN)
+# Space complexity : O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        l = 0
+        r = len(nums)-1
+        
+        while l <= r :
+            
+            mid = l + (r-l)//2
+            
+            if nums[mid] == target :
+                return mid
+            elif nums[l] <= nums[mid] :
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid-1
+                else :
+                    l = mid+1
+            else :
+                if target > nums[mid] and target <= nums[r] :
+                    l = mid+1
+                else :
+                    r = mid-1
+                    
+        return -1
+                    
