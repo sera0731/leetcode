2135aa64fab09761a9f53077385854b81b49204a
Sera Wi <win10790@gmail.com>
2020-05-18 20:15:07 -0400

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+# Time complexity : O(logN)
+# Space complexity : O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        l = 0
+        r = len(nums)-1
+        
+        idx = -1
+        
+        while l <= r :
+            
+            mid = l + (r-l)//2
+            
+            if nums[mid] == target :
+                idx = mid
+                break
+            elif nums[mid] > target :
+                r = mid-1
+            else :
+                l = mid+1
+                
+        return idx
