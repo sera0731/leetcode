7a5b61946e89dece8721dd66f638acbd15525a4a
Sera Wi <win10790@gmail.com>
2020-05-19 22:43:55 -0400

    Create bitwise.py

---
+++ bitwise.py
@@ -0,0 +1,14 @@
+# Time complexity : O(1)
+# Space complexity : O(1)
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        
+        shift = 0
+        
+        while m < n :
+            m >>= 1
+            n >>= 1
+            shift += 1
+            
+        return m<<shift
+        
