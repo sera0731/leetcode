fc051a6ae228a7777849424bb6e54fdf736a3268
Sera Wi <win10790@gmail.com>
2020-06-19 00:14:32 -0400

    Create palindrome_number.py

---
+++ leetcode/Array/palindrome_number.py
@@ -0,0 +1,18 @@
+# Time complexity : O(logN)
+# Space complexity : O(1)
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        if x < 0 :
+            return False
+        
+        y = x
+        rev = 0
+        
+        while y :
+            rev = rev * 10 + y % 10
+            y //= 10
+            
+        return rev == x
+        
