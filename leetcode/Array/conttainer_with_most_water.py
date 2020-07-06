e7d9e3786650d750b9850429acd7e405d863a336
Sera Wi <win10790@gmail.com>
2020-07-05 20:21:43 -0400

    Create conttainer_with_most_water.py

---
+++ leetcode/Array/conttainer_with_most_water.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        area = 0
+        
+        l = 0
+        r = len(height)-1
+        
+        while l < r :
+            
+            curr = (r-l)*(min(height[l], height[r]))
+            
+            if area < curr :
+                area = curr
+            
+            if height[l] < height[r] :
+                l += 1
+            else :
+                r -= 1
+                
+        return area
+        
