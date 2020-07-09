04054214545dc964af81e16135357622afc56bfa
Sera Wi <win10790@gmail.com>
2020-07-09 19:54:54 -0400

    Create num_trees.py

---
+++ leetcode/DynamicProgramming/num_trees.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        
+        dp = [0]*(n+1)
+        
+        dp[0] = 1
+        dp[1] = 1
+        
+        for m in range(2, n+1) :
+            for i in range(1, m+1) :
+                dp[m] += dp[i-1]*dp[m-i]
+            
+        return dp[-1]
+        
