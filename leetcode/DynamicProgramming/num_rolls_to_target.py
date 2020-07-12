97cd2b033f8fbd08897db68e36e36463af0525e3
Sera Wi <win10790@gmail.com>
2020-07-12 16:34:34 -0400

    Update num_rolls_to_target.py

--- leetcode/DynamicProgramming/num_rolls_to_target.py
+++ leetcode/DynamicProgramming/num_rolls_to_target.py
@@ -1,4 +1,6 @@
 class Solution:
+    
+    # Iterative
     def numRollsToTarget(self, d: int, f: int, target: int) -> int:
         
         mod = 10**9 + 7
@@ -12,4 +14,25 @@ class Solution:
                     dp[r][t] = (dp[r][t] + dp[r-1][t-curr]) % mod
                     
         return dp[d][target] % mod
+    
+     # Recursive
+     def numRollsToTarget(self, d: int, f: int, target: int) -> int:
         
+        memo = {}
+        
+        def helper(remain, t) :
+            
+            if remain == 0 :
+                return 0 if t > 0 else 1
+            if (remain, t) in memo :
+                return memo[(remain, t)]
+            
+            total = 0
+            
+            for i in range(max(0, t-f), t) :
+                total += helper(remain-1, i)
+            
+            memo[(remain, t)] = total
+            return total
+        
+        return helper(d, target)%(10**9+7)
