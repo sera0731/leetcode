2802b624339d97b0f9aa026498fbe81cc3e1919c
Sera Wi <win10790@gmail.com>
2020-07-16 18:04:14 -0400

    Update clone_graph.py

--- leetcode/Graph/clone_graph.py
+++ leetcode/Graph/clone_graph.py
@@ -1,5 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
 class Solution:
+    # DFS
+    def __init__(self):
+        self.nodes = {}
+        
     def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        if not node :
+            return node
+        
+        if node in self.nodes :
+            return self.nodes[node]
+        
+        clone = Node(node.val, [])
+        self.nodes[node] = clone
+        
+        if node.neighbors :
+            clone.neighbors = [self.cloneGraph(i) for i in node.neighbors]
+        
+        return clone
+    
+    # BFS
+    def cloneGraphBFS(self, node: 'Node') -> 'Node':
         
         if not node :
             return node
