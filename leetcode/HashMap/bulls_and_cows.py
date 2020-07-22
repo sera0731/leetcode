5b4184a3ee4d3e5e83375f02440f1fcffab815cf
Sera Wi <win10790@gmail.com>
2020-07-22 00:14:00 -0400

    Update bulls_and_cows.py

--- leetcode/HashMap/bulls_and_cows.py
+++ leetcode/HashMap/bulls_and_cows.py
@@ -20,3 +20,27 @@ class Solution:
                 
         return str(bull)+"A"+str(cow)+"B"
         
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        
+        nums = [0]*10
+        bull = cow = 0
+        
+        for i in range(len(secret)) :
+            
+            s = secret[i]
+            g = guess[i]
+            
+            if s == g :
+                bull += 1
+            else :
+                if nums[int(s)] < 0 :
+                    cow += 1
+                if nums[int(g)] > 0 :
+                    cow += 1
+                    
+                nums[int(s)] += 1
+                nums[int(g)] -= 1
+                
+        return '{}A{}B'.format(bull, cow)
+                
