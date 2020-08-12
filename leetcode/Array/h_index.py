9be3b7214210415c39e4b79d81591d12ebd3f5bd
Sera Wi <win10790@gmail.com>
2020-08-11 21:02:28 -0400

    Update h_index.py

--- leetcode/Array/h_index.py
+++ leetcode/Array/h_index.py
@@ -1,3 +1,4 @@
+# O(NlogN)
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         
@@ -13,3 +14,26 @@ class Solution:
                 return n-i
         
         return 0
+    
+# O(N)  
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        n = len(citations)
+        papers = [0]*(n+1) # i = 몇 번 참조, v = 몇 개
+        
+        for i in range(n) :
+            papers[min(citations[i], n)] += 1
+        
+        # c => [1, 3, 2, 3, 100]
+        # papers => [0, 1, 1, 2, 0, 1]
+        
+        h = n
+        s = papers[h]
+        
+        while h > s :
+            h -= 1
+            s += papers[h]
+            
+        return h
+        
