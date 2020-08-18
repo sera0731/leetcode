d9ea59ad42ce4aefee01f431a7028feb4a599ce1
Sera Wi <win10790@gmail.com>
2020-08-17 20:12:10 -0400

    Create decode_ways.py

---
+++ leetcode/String/decode_ways.py
@@ -0,0 +1,54 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        def helper(i) :
+            
+            if i == n :
+                return 1
+            
+            if  i > n or s[i] == "0":
+                return 0
+            
+            if i in memo :
+                return memo[i]
+            
+            first = 0
+            second = 0
+            
+            if int(s[i:i+1]) < 27 :
+                first = helper(i+1)
+            if int(s[i:i+2]) < 27 :
+                second = helper(i+2)
+                
+            memo[i] = first + second
+            return memo[i]
+            
+        n = len(s)
+        memo = {}
+        
+        return helper(0)
+        
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        if not s :
+            return 0
+        
+        n = len(s)
+        dp = [0]*(n+1)
+        
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
+        
+        for i in range(2, n+1):
+            
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            
+            curr = int(s[i-2:i])
+            
+            if 9 < curr < 27 :
+                dp[i] += dp[i-2]
+        
+        return dp[n]
+        
