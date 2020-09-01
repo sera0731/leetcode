6a84ce4b4454dd154e7331c67a6e76da03e846dc
Sera Wi <win10790@gmail.com>
2020-09-01 09:02:31 -0400

    Update largest_time_for_given_digits.py

--- leetcode/Backtracking/largest_time_for_given_digits.py
+++ leetcode/Backtracking/largest_time_for_given_digits.py
@@ -3,13 +3,13 @@ class Solution:
         
         output = -1
         
-        def helper(start, curr) :
+        def helper(start=0) :
             
             nonlocal output
             
             if start == len(A) :
-                h = curr[0]*10 + curr[1]
-                m = curr[2]*10 + curr[3]
+                h = A[0]*10 + A[1]
+                m = A[2]*10 + A[3]
                 
                 if h <= 23 and m <= 59 :
                     output = max(output, h*60+m)
@@ -17,9 +17,9 @@ class Solution:
             
             for i in range(start, len(A)) :
                 A[i], A[start] = A[start], A[i]
-                helper(start+1, A)
+                helper(start+1)
                 A[i], A[start] = A[start], A[i]
          
-        helper(0, A)
+        helper()
         return '' if output == -1 else '{:02d}:{:02d}'.format(output//60, output%60)
         
