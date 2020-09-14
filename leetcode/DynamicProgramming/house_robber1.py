430df8ed63062e9aacc4796ecba180c2d4c59752
Sera Wi <win10790@gmail.com>
2020-09-14 07:36:52 -0400

    Update house_robber1.py

--- leetcode/DynamicProgramming/house_robber1.py
+++ leetcode/DynamicProgramming/house_robber1.py
@@ -1,17 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        if len(nums) < 2 :
+            return 0 if not nums else nums[0]
+        
         n = len(nums)
-        
-        if n == 0 :
-            return 0
-        
-        dp = [0]*(n+1)
+        dp = [0]*n
         
         dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         
-        for i in range(1, n) :
+        for i in range(2, n) :
             dp[i] = max(dp[i-2]+nums[i], dp[i-1])
-            
-        return dp[n-1]
-            
+        
+        return dp[-1]
+    
