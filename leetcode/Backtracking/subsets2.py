580493550f7dd65c008fd64251e20d552689a2ff
Sera Wi <win10790@gmail.com>
2020-10-20 00:20:37 -0400

    Create subsets2.py

---
+++ leetcode/Backtracking/subsets2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        def helper(start, path) :
+            
+            output.append(path[:])
+            
+            for i in range(start, len(nums)) :
+                
+                if i > start and nums[i] == nums[i-1] :
+                    continue
+                
+                helper(i+1, path+[nums[i]])
+            
+        nums.sort()
+        output = []
+        helper(0, [])
+        return output
