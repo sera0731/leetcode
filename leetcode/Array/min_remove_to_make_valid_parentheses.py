7f571341415bc682e4a56315faaae116977384bd
Sera Wi <win10790@gmail.com>
2020-10-22 01:44:21 -0400

    Update min_remove_to_make_valid_parentheses.py

--- leetcode/Array/min_remove_to_make_valid_parentheses.py
+++ leetcode/Array/min_remove_to_make_valid_parentheses.py
@@ -24,3 +24,31 @@ class Solution:
                 
         return ''.join(output)
         
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        
+        filtered = []
+        l_total = l_extra = 0
+        
+        for ch in s :
+            
+            if ch == '(' :
+                l_total += 1
+                l_extra += 1
+            elif ch == ')' :
+                if l_extra == 0 :
+                    continue
+                l_extra -= 1
+                
+            filtered.append(ch)
+            
+        l_keep = l_total - l_extra
+        
+        for i, ch in enumerate(filtered) :
+            if ch == '(' :
+                l_keep -= 1
+                if l_keep < 0 :
+                    filtered[i] = ''
+                    
+        return ''.join(filtered)
+        
