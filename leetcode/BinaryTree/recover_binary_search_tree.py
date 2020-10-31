fe95011eedaa3388678b6ff099719942ab1e7322
Sera Wi <win10790@gmail.com>
2020-10-31 17:10:14 -0400

    Create recover_binary_search_tree.py

---
+++ leetcode/BinaryTree/recover_binary_search_tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        x = y = pred = None
+        stack = []
+        
+        while stack or root :
+            
+            while root :
+                stack.append(root)
+                root = root.left
+                
+            root = stack.pop()
+            if pred and root.val < pred.val :
+                y = root
+                if not x :
+                    x = pred
+                else :
+                    break
+            
+            pred = root
+            root = root.right
+        
+        x.val, y.val = y.val, x.val
+            
+                
