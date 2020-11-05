64be9b8568cced58bc0d97cd4c2b52252b2951ae
Sera Wi <win10790@gmail.com>
2020-11-05 08:45:42 -0500

    Update longest_repeating_character_replacement.py

--- leetcode/String/longest_repeating_character_replacement.py
+++ leetcode/String/longest_repeating_character_replacement.py
@@ -7,8 +7,9 @@ class Solution:
         for end in range(len(s)) :
             
             count[s[end]] += 1
-            max_cnt = max(max_cnt, count[s[end]])
+            max_cnt = max(max_cnt, count[s[end]]) 
             
+            # Replace 해야 하는 글자 수가 k보다 클 경우 start를 오른쪽으로 이동.
             if (end - start + 1) - max_cnt > k  :
                 count[s[start]] -= 1
                 start += 1
