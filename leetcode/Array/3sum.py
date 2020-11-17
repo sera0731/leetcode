b469ed106079ddb2be5f07294ca086fb3acccf20
Sera Wi <win10790@gmail.com>
2020-11-17 08:55:30 -0500

    Update 3sum.py

--- leetcode/Array/3sum.py
+++ leetcode/Array/3sum.py
@@ -1,32 +1,41 @@
-# Time complexity : O(N^2)
-# Space complexity : O(N)
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        if not nums or len(nums) < 3 :
-            return
+        output = []
         
-        ans = []
+        if not nums :
+            return output
+        
         nums.sort()
         
-        for i in range(len(nums)-2) :
+        i = 0
+        n = len(nums)
+        
+        for i in range(n-2) :
             
-            if i == 0 or nums[i-1] != nums[i] :
+            if i != 0 and nums[i-1] == nums[i] :
+                continue
+            
+            l = i+1
+            r = n-1
+            
+            while l < r :
                 
-                l, r = i+1, len(nums)-1
-                value = -nums[i]
+                res = nums[l] + nums[r] + nums[i]
                 
-                while l < r :
-                    result = nums[l] + nums[r]
-                    if result < value or (l-1> i and nums[l] == nums[l-1]) :
-                        l += 1
-                    elif result > value or (r+1 < len(nums) and nums[r] == nums[r+1]) :
-                        r -= 1
-                    elif result == value :
-                        ans.append([nums[i], nums[l], nums[r]])
-                        l += 1
-                        r -= 1
+                if res == 0 :
+                    output.append([nums[i], nums[l], nums[r]])
                     
-        return ans
-                 
+                    l += 1
+                    r -= 1
+                    
+                    while l<r and nums[l] == nums[l-1] : l += 1
+                    while l<r and nums[r] == nums[r+1] : r -= 1
+                        
+                elif res < 0 :
+                    l += 1
+                else :
+                    r -= 1
+                
+        return output
+                
