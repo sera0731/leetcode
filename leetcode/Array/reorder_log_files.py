074ced09c73916fe980dadbacb5c27834e487a4b
Sera Wi <win10790@gmail.com>
2020-11-17 21:03:42 -0500

    Update reorder_log_files.py

--- leetcode/Array/reorder_log_files.py
+++ leetcode/Array/reorder_log_files.py
@@ -1,9 +1,14 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-    
-        def order(log) :
-            id, rest = log.split(' ', 1)
-            return (0, rest, id) if rest[0].isalpha() else (1,)
+        
+        def sort(log) :
             
-        return sorted(logs, key=order)
+            identifier, rest = log.split(' ', 1)
+            
+            if rest[0].isnumeric() :
+                return (1, '')
+            
+            return (0, rest, identifier)
+
+        return sorted(logs, key=sort)
         
